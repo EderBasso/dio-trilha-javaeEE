f9aca18ac9b1ec62d01abde377205e66bfd215c6
eder.basso <eder.sdz@gmail.com>
2022-10-05 11:52:35 -0300

    Entrega final do 1 projeto Simulando Uma Conta Bancária Através Do Terminal/Console

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Projeto para entregas do curso de Java Enterprise da DiO");
+        System.out.println("Cada entrega está em uma branch diferente.");
+    }
+}
